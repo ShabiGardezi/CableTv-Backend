987afe0c591d53c786d64b0e8a7f70406799c81a
ShabiGardezi <shabigardezi51214@gmail.com>
2023-08-22 17:34:16 -0500

    optimized

--- index.js
+++ index.js
@@ -103,6 +103,7 @@ app.post("/api/login", (req, res) => {
 
   // Find the user with the provided email
   const user = users.find((u) => u.email === email);
+  console.log(user);
 
   if (!user || user.password !== password) {
     return res.status(401).json({ error: "Wrong email or password" });
