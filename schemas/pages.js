6ba91305a7496fa60ec035e95ee6051fae677e2a
ShabiGardezi <shabigardezi51214@gmail.com>
2023-09-14 13:58:55 -0500

    changed for live

--- schemas/pages.js
+++ schemas/pages.js
@@ -11,5 +11,8 @@ const PagesSchema = new mongoose.Schema({
   CableInternet_Section: {},
   SatelliteInternet_Section: {},
   SatelliteTv_Section: {},
+  FullBlog_1: {},
+  FullBlog_2: {},
+  FullBlog_3: {},
 });
 module.exports = mongoose.model("page", PagesSchema);
